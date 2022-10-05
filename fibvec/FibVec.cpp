8012f965ddb8e451aafb8b1b3054a0706a5a9f87
Jenniebn <jingyi_wu@umail.ucsb.edu>
2022-10-05 14:51:15 -0700

    fibvec

--- fibvec/FibVec.cpp
+++ fibvec/FibVec.cpp
@@ -6,20 +6,48 @@
 #include <stdexcept>
 using namespace std;
 
-// FibVec::FibVec(){
-//     v.reserve(1);
-//     cout << "constructor executed" << endl;
-// }
+FibVec::FibVec(){ // creates en empty vector w a capacity of one
+    n = 1;
+    vector = new int[n];
+    cout << "constructor executed" << endl; // REMOVE
+}
 
-// size_t FibVec::capacity(){
-//     return v.capacity();
-// }
+FibVec:: ~FibVec(){
+    cout << "destructor executed" << endl; // REMOVE
+    delete [] vector;
+}
 
-// size_t FibVec::count(){
-//     return v.size();
-// }
+size_t FibVec::capacity() const{ // returns the total size of the storage buffer
+    return n;
+}
 
+size_t FibVec::count() const{ // returns the number of items stored in the vector
+    return count;
+}
 
+void FibVec::insert(int value, size_t index){
+    int countNum = count();
+    if (index > countNum){
+        throw out_of_range("Out of range.");
+    }
+    else{
+        v[index] = value;
+    }
+}
+
+int FibVec::lookup(size_t index) const{
+    return a[index];
+}
+
+int FibVec::pop(){
+    if (v.size() == 0){
+
+    }
+}
+
+void push(int value);
+
+int remove(size_t index);
 
 int main( )
 {
--- fibvec/FibVec.h
+++ fibvec/FibVec.h
@@ -1,21 +1,22 @@
 #include <iostream> // for cout, cin
 #include <string>
 #include <algorithm>
-#include <vector>
 using namespace std;
 
 class FibVec{
     public:
         FibVec();
         ~ FibVec() {};
-        size_t capacity();
-        size_t count();
-        int insert(int value, size_t count);
+        size_t capacity() const;
+        size_t count() const;
+        void insert(int value, size_t index);
         int lookup(size_t index) const;
         int pop();
         void push(int value);
         int remove(size_t index);
+        FibVec slice(size_t index, size_t count);
     private:
-        vector<int> v;
+        int n;
+        int* vector;
 
 };
