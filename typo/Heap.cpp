bb423e2ea0823e8b95b55d693f1f3b60fa3dfc74
Jenniebn <jingyi_wu@umail.ucsb.edu>
2022-11-09 18:46:25 -0800

    new

--- typo/Heap.cpp
+++ typo/Heap.cpp
@@ -1,19 +1,27 @@
 #include "Heap.h"
+#include <iostream> 
+using namespace std;
 
 Heap::Heap(size_t capacity){
-    Entry* myHeap[capacity];
+    mCount = 0;
+    mCapacity = capacity;
+    mData = new Heap::Entry[capacity];
 }
 
 Heap::Heap(const Heap& other){
-
+    mData = new Heap::Entry[other.capacity()];
+    for (size_t i = 0; i < other.count(); i++){
+        mData[i] = other.mData[i];
+    }
 }
 
 Heap::Heap(Heap&& other){
-
+    mData = other.mData;
+    other.mData = NULL;
 }
 
 Heap::~Heap(){
-
+    delete mData;
 }
 
 size_t       Heap::capacity() const{
@@ -25,21 +33,85 @@ size_t       Heap::count() const{
 }
 
 const Heap::Entry& Heap::lookup(size_t index) const{
-
+    if (index > count() - 1){
+        throw out_of_range("Out of Range");
+    }
+    else{
+        return mData[index];
+    }
 }
 
 Heap::Entry        Heap::pop(){
-
+    if (count() == 0){
+        throw underflow_error("Underflow Error");
+    }
+    else{
+        Heap::Entry lowest = mData[0];
+        Heap::Entry last = mData[count() - 1];
+        mData[0] = last;
+        mCount --;
+        size_t index = 0;
+        size_t leftChild = index * 2 + 1;
+        size_t rightChild = index * 2 + 2;
+        while (index < count()){
+            leftChild = index * 2 + 1;
+            rightChild = index * 2 + 2;
+            if (leftChild < count() && rightChild < count()){
+                if (mData[leftChild].score > mData[rightChild].score){
+                    Heap::Entry temp = mData[rightChild];
+                    mData[rightChild] = mData[index];
+                    mData[index] = temp;
+                    index = rightChild;
+                }
+            }
+            else{
+                Heap::Entry temp = mData[leftChild];
+                mData[leftChild] = mData[index];
+                mData[index] = temp;
+                index = leftChild;
+            }
+        }
+        return lowest;
+    }
 }
 
-Heap::Entry        Heap::pushpop(const std::string& value, float score){
-
-}
+// Heap::Entry        Heap::pushpop(const std::string& value, float score){
+//     Heap::Entry temp = mData[0];
+//     return temp;
+// }
 
 void         Heap::push(const std::string& value, float score){
-
+    if (count() >= capacity()){
+        throw overflow_error("Overflow Error");
+    }
+    Heap::Entry newEntry;
+    newEntry.value = value;
+    newEntry.score = score;
+    
+    mData[count()] = newEntry;
+    
+    size_t index = count();
+    mCount++;
+    
+    while(index >= 0){
+        size_t parent = (index - 1) / 2;
+        if (mData[parent].score <= mData[index].score){
+            break;
+        }
+        else{
+            Heap::Entry temp = mData[parent];
+            mData[parent] = mData[index];
+            mData[index] = temp;
+            index = parent;
+        }
+    }
 }
 
 const Heap::Entry& Heap::top() const{
-
+    if (count() == 0){
+        throw underflow_error("Underflow Error");
+    }
+    else{
+        return mData[0];
+    }
 }
--- typo/test.cpp
+++ typo/test.cpp
@@ -1,13 +1,22 @@
 #include "Heap.h"
 #include <iostream>
+using namespace std;
 
 // Use this file to test your Heap class!
 // This file won't be graded - do whatever you want.
 
 int main() {
     Heap heap(8);
-
-    std::cout << "This program doesn't do anything interesting yet...\n";
+    heap.push("5", 5);
+    heap.push("3", 3);
+    heap.push("1", 1);
+    cout << heap.top().value << endl;
+    
+    heap.push("9", 9);
+    heap.push("6", 6);
+    heap.push("8", 8);
+    heap.push("2", 2);
+    
 
     return 0;
 }
