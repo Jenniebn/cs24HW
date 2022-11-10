63714e1b174a2c10aebc28085e76110bf65c4423
Jenniebn <jingyi_wu@umail.ucsb.edu>
2022-11-10 11:30:52 -0800

    new

--- typo/Heap.cpp
+++ typo/Heap.cpp
@@ -39,10 +39,9 @@ size_t       Heap::count() const{
 }
 
 const Heap::Entry& Heap::lookup(size_t index) const{
-    size_t zero = 0;
     // std::cout << "index = " << index << std::endl;
     // std::cout << "count() = " << count() << std::endl;
-    if ((index > count() - 1) || (index < zero)){
+    if (index >= count()){
         throw out_of_range("Out of Range");
     }
     else{
